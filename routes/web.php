2cd35bb0cbeb492c9bc29a4f22cc8f2d49240331
abdoulahi21 <abdoulahi.diallo@unchk.edu.sn>
2025-07-20 11:33:58 +0000

    views recruteur

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.admin.template');
 });
 
 Route::get('/dashboard', function () {
